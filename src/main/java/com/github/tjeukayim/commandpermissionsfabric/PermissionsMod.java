d7af6954d4c610fc8923b8f6a0651e58d8bc3a68
Tjeu Kayim <15987676+TjeuKayim@users.noreply.github.com>
2021-06-07 22:48:17 +0200

    Use NodeMutateEvent again

--- src/main/java/com/github/tjeukayim/commandpermissionsfabric/PermissionsMod.java
+++ src/main/java/com/github/tjeukayim/commandpermissionsfabric/PermissionsMod.java
@@ -5,14 +5,22 @@ import me.lucko.fabric.api.permissions.v0.Permissions;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.luckperms.api.LuckPerms;
 import net.luckperms.api.LuckPermsProvider;
-import net.luckperms.api.event.user.UserDataRecalculateEvent;
+import net.luckperms.api.event.node.NodeMutateEvent;
+import net.luckperms.api.model.group.Group;
+import net.luckperms.api.model.user.User;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.ServerTask;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.server.network.ServerPlayerEntity;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.Objects;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -22,6 +30,7 @@ public class PermissionsMod implements ModInitializer {
      */
     public static final String PREFIX = "minecraft.command.";
     private static final Logger LOGGER = LogManager.getLogger();
+    private final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
 
     @Override
     public void onInitialize() {
@@ -40,11 +49,20 @@ public class PermissionsMod implements ModInitializer {
             LOGGER.info("Loaded Minecraft Command Permissions");
         });
         ServerLifecycleEvents.SERVER_STARTED.register(server -> {
-            LuckPermsProvider.get().getEventBus().subscribe(UserDataRecalculateEvent.class, e -> {
-                ServerPlayerEntity player = server.getPlayerManager().getPlayer(e.getUser().getUniqueId());
-                if (player != null) {
-                    server.getPlayerManager().sendCommandTree(player);
-                    LOGGER.debug(() -> "Updated command tree for " + player.getName());
+            LuckPerms luckPerms = LuckPermsProvider.get();
+            luckPerms.getEventBus().subscribe(NodeMutateEvent.class, e -> {
+                if (e.isUser()) {
+                    var user = (User) e.getTarget();
+                    onPermissionsChange(server, user);
+                } else if (e.isGroup()) {
+                    var group = (Group) e.getTarget();
+                    // Delay because user is not yet updated otherwise
+                    Runnable updateGroup = () ->
+                            server.send(new ServerTask(1, () -> luckPerms.getUserManager().getLoadedUsers()
+                                    .stream()
+                                    .filter(u -> u.getInheritedGroups(u.getQueryOptions()).contains(group))
+                                    .forEach(u -> onPermissionsChange(server, u))));
+                    executorService.schedule(updateGroup, 1, TimeUnit.SECONDS);
                 }
             });
             LOGGER.info("Set-up autocompletion refresh for LuckPerms");
@@ -86,5 +104,14 @@ public class PermissionsMod implements ModInitializer {
             }
         }
         return null;
+    }
+
+    private void onPermissionsChange(MinecraftServer server, User user) {
+        // TODO: Sometimes this event fires multiple times at once
+        ServerPlayerEntity player = server.getPlayerManager().getPlayer(user.getUniqueId());
+        if (player != null) {
+            server.getPlayerManager().sendCommandTree(player);
+            LOGGER.debug(() -> "Updated command tree for " + player.getName());
+        }
     }
 }
